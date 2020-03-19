44ccb005b75cb9340863419fc7cf7ef5636cc24b
Jiang Wu <john.x.wu@ericsson.com>
2020-03-19 09:53:50 +0200

    complatedFuture study 1

--- src/main/java/com/example/reactor/java8/completableFuture/CompletableFuture.java
+++
@@ -1,4 +0,0 @@
-package com.example.reactor.java8.completableFuture;
-
-public class CompletableFuture {
-}
---
+++ src/test/java/com/example/reactor/java8/completableFuture/CompletableFutureDemoTest.java
@@ -0,0 +1,12 @@
+package com.example.reactor.java8.completableFuture;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CompletableFutureDemoTest {
+
+    @Test
+    void test1() {
+    }
+}
